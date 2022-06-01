e8d098c7641ca194dbab553919c3fde9ed999c7b
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2022-06-01 19:09:59 +0300

    Классу исключения добавлена проверка сообщения на null

--- src/main/java/dev/kalenchukov/fieldvaluechanger/exceptions/InvalidChangerClassException.java
+++ src/main/java/dev/kalenchukov/fieldvaluechanger/exceptions/InvalidChangerClassException.java
@@ -6,26 +6,24 @@
 
 package dev.kalenchukov.fieldvaluechanger.exceptions;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
 /**
  * Исключение для некорректного изменяющего значение поля.
  */
 public class InvalidChangerClassException extends RuntimeException
 {
     /**
-     * Конструктор для {@code InvalidChangerFieldValueChangerException}.
-     */
-    public InvalidChangerClassException()
-    {
-        super();
-    }
-
-    /**
-     * Конструктор для {@code InvalidChangerFieldValueChangerException}.
+     * Конструктор для {@code InvalidChangerException}.
      *
      * @param message Сообщение.
      */
-    public InvalidChangerClassException(String message)
+    public InvalidChangerClassException(@NotNull final String message)
     {
         super(message);
+
+		Objects.requireNonNull(message);
     }
 }
